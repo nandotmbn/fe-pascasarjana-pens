def299d9fd3a908200cbe58069402286fd3361e2
nandotmbn <orlandopratama695@gmail.com>
2023-02-14 09:37:49 +0700

    Init Commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux'
+import { store } from 'src/utils/redux/store'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: any) {
+  return (
+    <Provider store={store}>
+			<Component {...pageProps} />
+		</Provider>
+  )
 }
