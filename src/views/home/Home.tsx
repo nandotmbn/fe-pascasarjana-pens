bff11a03f7f6e3ff25e525b374c7fb95e59565b6
nandotmbn <orlandopratama695@gmail.com>
2023-02-27 01:12:22 +0700

    feat: news home

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,6 +1,7 @@
 import React, { CSSProperties, useState } from "react";
 import Announcements from "./partials/Announcements";
 import Jumbotron from "./partials/Jumbotron";
+import News from "./partials/News";
 
 function HomeViews() {
 	const [searchText, setSearchText] = useState("");
@@ -8,6 +9,7 @@ function HomeViews() {
 		<div className={`w-full flex flex-col`}>
 			<Jumbotron />
 			<Announcements />
+			<News />
 		</div>
 	);
 }
--- src/views/home/partials/Announcements.tsx
+++ src/views/home/partials/Announcements.tsx
@@ -5,8 +5,8 @@ import React from "react";
 function AnnouncementsCard() {
 	return (
 		<div className="inline-block px-3">
-			<div className="w-64 h-96 max-w-xs overflow-hidden rounded-lg drop-shadow-xl bg-white hover:drop-shadow-2xl transition-shadow duration-300 ease-in-out border-t-4 border-t-blue-800 border-r-4 border-r-yellow-400 px-4 pt-2 py-8 flex flex-col justify-between">
-				<div>
+			<div className="w-64 max-w-xs overflow-hidden rounded-lg drop-shadow-xl bg-white hover:drop-shadow-2xl transition-shadow duration-300 ease-in-out border-t-4 border-t-blue-800 border-r-4 border-r-yellow-400 px-4 pt-2 flex flex-col justify-between">
+				<div className="flex-3">
 					<h5 className="font-semibold text-2xl">
 						Lorem ipsum dolor sit amet consectetur adipisicing elit.
 					</h5>
@@ -15,7 +15,7 @@ function AnnouncementsCard() {
 						consectetur veritatis aut.
 					</p>
 				</div>
-				<div className="self-end">
+				<div className="self-end flex-1 pb-4 mt-8">
 					<Link href="/article" className="flex flex-row items-center gap-2">
 						<p className="text-blue-800 text-sm">Baca Selengkapnya...</p>
 						<button className="w-6 h-6 rounded-full bg-blue-800">
